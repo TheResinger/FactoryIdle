21c31fea73e6b271aae7bfd39a17c73cce670b75
Benjamin Morrow <morrowbenjamin42@gmail.com>
2021-03-01 13:59:42 -0500

    apparlentlty github removed master from being the default branch

---
+++ src/App.js
@@ -0,0 +1,89 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm" style={{ padding: '0px' }}>
+            <div
+              className="card"
+              style={{
+                marginTop: '5px',
+                marginLeft: '5px',
+                marginRight: '5px',
+              }}
+            >
+              <div className="card-body">
+                <h3 className="text-center">Raw Resources</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm" style={{ padding: '0px' }}>
+            <div
+              className="card"
+              style={{
+                marginTop: '5px',
+                marginLeft: '5px',
+                marginRight: '5px',
+              }}
+            >
+              <div className="card-body">
+                <h4 className="card-title text-center">Iron Ore</h4>
+                <p className="card-text">Some quick text</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm" style={{ padding: '0px' }}>
+            <div
+              className="card"
+              style={{
+                marginTop: '5px',
+                marginLeft: '5px',
+                marginRight: '5px',
+              }}
+            >
+              <div className="card-body">
+                <h4 className="card-title text-center">Copper Ore</h4>
+                <p className="card-text">Some quick text</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm" style={{ padding: '0px' }}>
+            <div
+              className="card"
+              style={{
+                marginTop: '5px',
+                marginLeft: '5px',
+                marginRight: '5px',
+              }}
+            >
+              <div className="card-body">
+                <h4 className="card-title text-center">Stone</h4>
+                <p className="card-text">Some quick text</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm" style={{ padding: '0px' }}>
+            <div
+              className="card"
+              style={{
+                marginTop: '5px',
+                marginLeft: '5px',
+                marginRight: '5px',
+              }}
+            >
+              <div className="card-body">
+                <h4 className="card-title text-center">Coal</h4>
+                <p className="card-text">Some quick text</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
